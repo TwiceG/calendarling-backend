d0e878023ebfb908bce0f38435a005e0b320a883
TwiceG <g0stron0g@gmail.com>
2025-02-24 14:05:54 +0100

    add breeze api for user functions

--- routes/api.php
+++ routes/api.php
@@ -1,24 +1,8 @@
 <?php
 
-use App\Http\Controllers\NoteController;
-use App\Models\QueryRepositories\NoteRepository;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-
-// Route::get('/test', function () {
-//     return response()->json(['message' => 'API is working!']);
-// });
-
-// Route::get('/', function () {
-//     return response()->json(['message' => 'Root is working!']);
-// });
-
-// Route::get('/test-notes/{date}', [NoteRepository::class, 'getNotes']);
-
-Route::get('/week-notes', [NoteController::class, 'getWeekNotes']);
-Route::post('/add-note', [NoteController::class, 'addNote']);
-Route::get('/get-note', [NoteController::class, 'getNote']);
-Route::delete('/delete-note', [NoteController::class, 'deleteNote']);
-
-
-Route::post('/send-daily-note-email', [NoteController::class, 'triggerEmailCheck']);
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+    return $request->user();
+});

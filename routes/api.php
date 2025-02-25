8f65e0ac969606983165e8c0ab750cff97b2c7e6
TwiceG <g0stron0g@gmail.com>
2025-02-25 15:37:16 +0100

    add middleware protection to routes

--- routes/api.php
+++ routes/api.php
@@ -6,11 +6,6 @@ use App\Http\Controllers\NoteController;
 use App\Http\Controllers\UserController;
 
 
-// Routes for NoteController
-Route::get('/week-notes', [NoteController::class, 'getWeekNotes']);
-Route::post('/add-note', [NoteController::class, 'addNote']);
-Route::get('/get-note', [NoteController::class, 'getNote']);
-Route::delete('/delete-note', [NoteController::class, 'deleteNote']);
 Route::post('/send-daily-note-email', [NoteController::class, 'triggerEmailCheck']);
 
 // Routes for UserController (authentication related)
@@ -20,3 +15,11 @@ Route::post('/login', [UserController::class, 'login']);
 // Protected routes
 Route::middleware(['auth:sanctum'])->get('/me', [UserController::class, 'me']);
 Route::middleware('auth:sanctum')->post('/logout', [UserController::class, 'logout']);
+
+// Routes for NoteController
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/week-notes', [NoteController::class, 'getWeekNotes']);
+    Route::post('/add-note', [NoteController::class, 'addNote']);
+    Route::get('/get-note', [NoteController::class, 'getNote']);
+    Route::delete('/delete-note', [NoteController::class, 'deleteNote']);
+});

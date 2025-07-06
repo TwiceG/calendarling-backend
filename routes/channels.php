7f04c06d105459a01baec4b20feb54d21f65f262
TwiceG <g0stron0g@gmail.com>
2025-07-06 17:51:05 +0200

    add channels for Routing in bootstrap app.php

--- routes/channels.php
+++ routes/channels.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Broadcast;
+use Illuminate\Support\Facades\Log;
 
 Broadcast::channel('chat.{id}', function ($user, $id) {
+    Log::info('Channel auth check', ['user' => $user, 'id' => $id]);
     return (int) $user->id === (int) $id;
 });
